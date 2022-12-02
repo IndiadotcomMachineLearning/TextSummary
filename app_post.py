7f7ddd21ba3e22d1513faf902f7404a0f2d536cb
ashish.khandelwal <ezmallpoc@gmail.com>
2022-12-02 17:47:28 +0530

    adding loggere

--- app_post.py
+++ app_post.py
@@ -7,15 +7,36 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from transformers import AutoTokenizer
 from transformers import pipeline
+import uvicorn
 import logging
 import json
+import logging
 
 
-model = AutoModelForSeq2SeqLM.from_pretrained(
-        "C:\\Text_Summary\\model")
+# model = AutoModelForSeq2SeqLM.from_pretrained(
+#         "./TEXT_SUMMARY_MODEL")
+#
+# tokenizer = AutoTokenizer.from_pretrained(
+#         "./TEXT_SUMMARY_MODEL")
+#
+# tokenizer.save_pretrained("C:\\Users\\Ashish.khandelwal\\ProjectWorkspace\\TextSummary\\TEXT_SUMMARY_MODEL_LOCAL")
+# model.save_pretrained("C:\\Users\\Ashish.khandelwal\\ProjectWorkspace\\TextSummary\\TEXT_SUMMARY_MODEL_LOCAL")
 
-tokenizer = AutoTokenizer.from_pretrained(
-        "C:\\Text_Summary\\model")
+
+# tokenizer = AutoTokenizer.from_pretrained("C:\\Users\\Ashish.khandelwal\\ProjectWorkspace\\TextSummary\\TEXT_SUMMARY_MODEL_LOCAL")
+# model = AutoModelForSeq2SeqLM.from_pretrained("C:\\Users\\Ashish.khandelwal\\ProjectWorkspace\\TextSummary\\TEXT_SUMMARY_MODEL_LOCAL")
+
+
+logger = logging.getLogger(__name__)
+
+tokenizer = AutoTokenizer.from_pretrained("TestZee/t5-small-finetuned-pytorch-final")
+model = AutoModelForSeq2SeqLM.from_pretrained("TestZee/t5-small-finetuned-pytorch-final")
+
+# tokenizer = AutoTokenizer.from_pretrained("my_model")
+# model = AutoModelForSeq2SeqLM.from_pretrained("my_model")
+
+
+
 
 class Item(BaseModel):
     article: str
@@ -26,8 +47,16 @@ app = FastAPI()
 @app.post("/items")
 def read_item(item: Item):
     try:
+        logger.info("Entered into try")
+        print(("Entered into try"))
+
+        logger.info("creating summary pipeline")
+        print(("creating summary pipeline"))
 
         summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
+
+        logger.info("summary pipeline created")
+        print(("summary pipeline created"))
 
         return(summarizer(item.article))
 
@@ -35,3 +64,7 @@ def read_item(item: Item):
     except Exception as e:
 
         return (str(e))
+
+
+if __name__== '__main__':
+    uvicorn.run(app, port=8000, host='0.0.0.0')
